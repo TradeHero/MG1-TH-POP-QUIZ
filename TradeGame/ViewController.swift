b53e11c138bdd81f9d2ffcc7ea3e8fb980c28951
Ryne Cheow <rynecheow@gmail.com>
2014-07-30 11:13:53 +0800

    Initial Commit

---
+++ TradeGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TradeGame
+//
+//  Created by Ryne Cheow on 7/30/14.
+//  Copyright (c) 2014 TradeHero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
