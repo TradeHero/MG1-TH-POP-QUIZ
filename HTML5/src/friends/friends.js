7a2f8872a6aec69d6a00f40714cb5485741a1198
Ryne Cheow <rynecheow@gmail.com>
2015-02-27 10:21:54 +0800

    #update - new pages
    Signed-off-by: Ryne Cheow <rynecheow@gmail.com>

---
+++ HTML5/src/friends/friends.js
@@ -0,0 +1,6 @@
+/**
+ * Created by rynecheow on 26/2/15.
+ */
+window.onload = {
+
+}
---
+++ HTML5/src/index/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rynecheow on 26/2/15.
+ */
+var quickGameButton = document.getElementById("qgame-btn");
+var friendsButton = document.getElementById("friends-btn");
+
+friendsButton.onclick = function(){
+    window.location.href = "http://0.0.0.0:8000/friends.html"
+};
+
+quickGameButton.onclick = function(){
+    window.location.href = "http://0.0.0.0:8000/friends.html"
+};
